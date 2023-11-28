9415f88c25b0f79d44d551c84ba0cb50e1614dd0
univernik77 <146747152+univernik77@users.noreply.github.com>
2023-11-28 16:19:56 +0300

    add description to pokemon

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -8,6 +8,8 @@ class Pokemon(models.Model):
     title_en = models.CharField(max_length=200, null=True, blank=True)
     title_jp = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(max_length=1000, null=True, blank=True)
+
+
     def __str__(self):
         return f'{self.title}'
 
